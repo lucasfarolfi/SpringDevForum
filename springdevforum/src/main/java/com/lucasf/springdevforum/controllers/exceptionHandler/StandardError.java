f45a9e8e5481880b405ebdd833fe9bab0e50683a
Lucas Farolfi <49098049+lucasfarolfi@users.noreply.github.com>
2022-06-19 01:30:55 -0300

    fix(exceptions): add exception handler in controller

---
+++ springdevforum/src/main/java/com/lucasf/springdevforum/controllers/exceptionHandler/StandardError.java
@@ -0,0 +1,15 @@
+package com.lucasf.springdevforum.controllers.exceptionHandler;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class StandardError {
+    private Long timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+}
